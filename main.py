12386771be9a09757de70dd8fe1e1f01cb1ceb7c
Tracie Johnson <johnsont5595@gmail.com>
2025-04-27 21:02:13 -0400

    Create the WeatherData class and create instance variables.

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+class WeatherData:
+    '''This class holds historical weather data for a set location and date.'''
 
-
-print("Hi There")
+    def __init__(self, latitude, longitude, month, day_of_month, year):
+        '''
+        Initialize WeatherData with the location and date information. The weather attributes will
+        be added later. latitude (float), longitude (float), month (int), day_of_month (int),
+        year (int)
+        '''
+        self.latitude = latitude
+        self.longitude = longitude
+        self.month = month
+        self.day_of_month = day_of_month
+        self.year = year
+        self.five_yr_avg_temp = None
+        self.five_yr_min_temp = None
+        self.five_yr_max_temp = None
+        self.five_yr_avg_wind_speed = None
+        self. five_yr_min_wind_speed = None
+        self.five_yr_max_wind_speed = None
+        self.five_yr_sum_precipiation = None
+        self.five_yr_min_precipiation = None
+        self.five_yr_max_precipiation = None
