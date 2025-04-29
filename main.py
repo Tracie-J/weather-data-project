89510891ad7beb899686085a3737ef58f708cadc
Tracie Johnson <johnsont5595@gmail.com>
2025-04-28 22:03:33 -0400

    Import requests and json. Create method to pull weather data from weather api. Start on section C2.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import requests
+import json
+
 class WeatherData:
-    '''This class holds historical weather data for a set location and date.'''
+    '''This class holds historical weather data for a Wilmington, NC on Sept 13th.'''
 
     def __init__(self, latitude, longitude, month, day_of_month, year):
         '''
@@ -16,8 +19,42 @@ class WeatherData:
         self.five_yr_min_temp = None
         self.five_yr_max_temp = None
         self.five_yr_avg_wind_speed = None
-        self. five_yr_min_wind_speed = None
+        self.five_yr_min_wind_speed = None
         self.five_yr_max_wind_speed = None
-        self.five_yr_sum_precipiation = None
-        self.five_yr_min_precipiation = None
-        self.five_yr_max_precipiation = None
+        self.five_yr_sum_precipitation = None
+        self.five_yr_min_precipitation = None
+        self.five_yr_max_precipitation = None
+
+
+# write a method for mean temp in Fahrenheit, max wind speed in miles per hour, and precipitation sum in inches.
+def fetch_weather_data(latitude, longitude, date):
+    url = 'https://archive-api.open-meteo.com/v1/archive'
+
+    params = {
+        'latitude': latitude,
+        'longitude': longitude,
+        'start_date': date,
+        'end_date': date,
+        'daily': ['temperature_2m_mean', 'wind_speed_10m_max', 'precipitation_sum'],
+        'timezone': 'America/New_York' #timezone for Wilmington, NC
+    }
+
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(f'Failed to fetch data: {response.status_code}')
+        return None
+
+latitude = 34.225727
+longitude = -77.944710
+date = '2024-09-13'
+
+weather_data = fetch_weather_data(latitude, longitude, date)
+print(weather_data)
+
+
+# write a method for maximum wind speed in miles per hour
+
+# write a method for precipitation sum in inches
