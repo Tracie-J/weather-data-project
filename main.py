36382aa8206ffb676ec605f11828141d8b94bc48
Tracie Johnson <johnsont5595@gmail.com>
2025-05-05 18:58:06 -0400

    Condensed five-year data method to generate calculations for all temp, wind, and precipitation aggregates.

--- main.py
+++ main.py
@@ -4,32 +4,21 @@ if __name__ == '__main__':
     # Create instance for Wilmington, NC event date 09/13/2025
     wilmington = WeatherData(34.225727, -77.944710, 9, 13, 2025)
 
-    # Populate temperature attributes
-    wilmington.five_year_avg_temp()
-    wilmington.five_year_min_temp()
-    wilmington.five_year_max_temp()
+    # Populate temperature, wind speed, and precipitation attributes
+    wilmington.five_year_data()
 
-    # Populate wind speed attributes
-    wilmington.five_year_avg_wind()
-    wilmington.five_year_min_wind()
-    wilmington.five_year_max_wind()
-
-    # Populate precipitation attributes
-    wilmington.five_year_precipitation_sum()
-    wilmington.five_year_precipitation_min()
-    wilmington.five_year_precipitation_max()
 
 
 '''Test that I can populate and print accurate data from the weather_data file'''
-    # print results
-    print(wilmington.five_year_avg_temp())
-    print(wilmington.five_year_min_temp())
-    print(wilmington.five_year_max_temp())
+# print results
+print(wilmington.five_year_data())
 
-    print(wilmington.five_year_avg_wind())
-    print(wilmington.five_year_min_wind())
-    print(wilmington.five_year_max_wind())
+'''test results
+Before conversions:
+{'avg_temp': 24.38, 'min_temp': 23.0, 'max_temp': 25.9, 'avg_wind': 18.32, 'min_wind': 9.8, 'max_wind': 30.2, 
+'sum_precip': 99.5, 'min_precip': 0.0, 'max_precip': 92.2}
 
-    print(wilmington.five_year_precipitation_sum())
-    print(wilmington.five_year_precipitation_min())
-    print(wilmington.five_year_precipitation_max())
+After conversions:
+{'avg_temp': 75.88, 'min_temp': 73.4, 'max_temp': 78.62, 'avg_wind': 11.38, 'min_wind': 6.09, 'max_wind': 18.77, 
+'sum_precip': 3.92, 'min_precip': 0.0, 'max_precip': 3.63}
+'''
